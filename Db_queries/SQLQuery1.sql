7c380bd34a079b68e706c3827402bb52d59e6d20
Arvind-mocha <arvind-mocha@users.noreply.github.com>
2020-06-14 12:47:50 +0530

    database connection with singup succeded with minor errors to consider

--- Db_queries/SQLQuery1.sql
+++ Db_queries/SQLQuery1.sql
@@ -1,9 +1,12 @@
 --Execute Once to create singup table --
-create table singup(userid int identity constraint pk_user primary key ,username varchar(25)
+create table singup(userid int constraint pk_user primary key ,username varchar(25)
 ,email varchar(30),password varchar(20),date_of_creation date,time_of_creation time)
 
 --Execute to view singup table content--
 select * from singup
+
+insert into [dbo].[singup] 
+values('271','arvind','arvind@yahoo.com','Clash!!12','2019-12-11','12:11:11')
 
 --Execute to remove singup table contents--
 truncate table singup
@@ -11,7 +14,8 @@ truncate table singup
 --Execute to remove singup table --
 drop table singup
 
-delete from [dbo].[singup] where userid=4
+delete from [dbo].[singup] where userid = 169
+
 
 
 
--- README.md
+++ README.md
@@ -6,4 +6,16 @@ FEEL FREE TO USE MY CODE IF NEED
 I HAVE ATTACHED EACH AND EVERY SINGLE PART YOU NEED TO RUN MY CODE
 
 PREQUSITES 
+  
   Install the required packages to avoid error
+  Run these lines on your Anacondaprompt or command prompt one by one(Mandatory)
+
+    pip install kivymd
+
+    pip install kivy
+
+    pip install pyodbc
+
+    pip install datetime
+
+    pip install re
