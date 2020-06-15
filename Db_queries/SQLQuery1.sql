57745a6b33ac79845e8b9f173e0953fa0fdccd93
Arvind-mocha <arvind-mocha@users.noreply.github.com>
2020-06-15 21:46:55 +0530

    login page completed

--- Db_queries/SQLQuery1.sql
+++ Db_queries/SQLQuery1.sql
@@ -1,17 +1,22 @@
---Execute Once to create singup table --
+--Execute Once to create table --
 create table singup(userid int constraint pk_user primary key ,username varchar(25)
 ,email varchar(30),password varchar(20),date_of_creation date,time_of_creation time)
+
+create table singin(userid int constraint fk_uid foreign key references singup(userid),logged_in_as varchar(20)
+,time_of_login time,date_of_login date)
+
+create table singupdummy(username varchar(20),password varchar(20)) 
 
 --Execute to view singup table content--
 select * from singup
 
---Execute to remove singup table contents--
-truncate table singup
+select * from singin 
 
---Execute to remove singup table --
-drop table singup
+select * from singupdummy
 
-insert into [dbo].[singup] values(123,'arvind','arvind@gmail.com','Arvind@123','2019-10-12','08:10:20')
+
+
+
 
 
 
